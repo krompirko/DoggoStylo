38d995b223f9299df0e47aa78538912265768b28
krompirko <laznimail10@gmail.com>
2018-09-29 15:59:54 +0200

    kurac commit

--- DoggoStylo2.0/DoggoStylo2.0/AppDelegate.swift
+++ DoggoStylo2.0/DoggoStylo2.0/AppDelegate.swift
@@ -23,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print("Nisam videla i nemogu da komentarisem")
         }
         DBDebugToolkit.add(imamCerkuNajLepsuMariju)
+        print("kurac")
         return true
     }
 
